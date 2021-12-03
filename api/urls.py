cbdf96853645270bff2caf64a5621146cede2274
Kiel_Hyre <vinceadisip@gmail.com>
2021-12-03 19:11:24 +0800

    cbvs

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list/', views.api, name='api-index'),
-    path('view/<str:pk>/', views.api_view, name='api-view'),
+    # path('list/', views.api, name='api-index'),
+    path('list/', views.APIListView.as_view(), name='api-index'),
+    # path('view/<str:pk>/', views.api_view, name='api-view'),
+    path('view/<str:pk>/', views.APIGetView.as_view(), name='api-view'),
     path('delete/<str:pk>/', views.APIDeleteView.as_view(), name='api-delete'),
     path('add/', views.APICreateView.as_view(), name='api-add'),
     path('update/<str:pk>/', views.APIUpdateView.as_view(), name='api-update'),
--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response # NOTE THIS INSTEAD OF HTTPRESPONSE OR RENDER we use this
+from rest_framework.views import APIView
+
 from django.views.decorators.csrf import csrf_exempt
 # https://www.django-rest-framework.org/api-guide/views/
 
@@ -9,30 +11,47 @@ from rest_framework.parsers import JSONParser
 from . import models
 from . import serializers # note were not using forms
 
-@api_view() # GET by default  @api_view(['GET'])
-def api(request):
-    context = {
+
+# v1
+# @api_view() # GET by default  @api_view(['GET'])
+# def api(request):
+#     context = {
+#         'name': 'Miguel',
+#         #'to_do': models.Action.objects.all(), # this will cause an error, all data should be serialize or non-object
+#         'to_do': models.Action.objects.all().values('id', 'text', 'description')
+
+#         # another approach was to use serialize as outside response
+#         # 'to_do': serializers.ActionSerializer(models.Action.objects.all(), many=True).data
+#         # https://www.django-rest-framework.org/api-guide/serializers/
+#         # this approach is much better
+#     }
+#     return Response(context) # status by default is 200
+
+# @api_view()
+# def api_view(request, pk):
+#     obj = get_object_or_404(models.Action, pk=pk)
+#     # NOTE IN THIS FUNCTION WERE ONLY FOCUSING ON TRANSFERRING BACK A SERIALIZED OBJECT
+#     # AND NOT WORRYING ABOUT TEMPLATES OR REDIRECTS
+#     return Response({'data':serializers.ActionSerializer(obj).data})
+
+
+class APIListView(APIView):
+    def get(self, request):
+        context = {
         'name': 'Miguel',
-        #'to_do': models.Action.objects.all(), # this will cause an error, all data should be serialize or non-object
-        'to_do': models.Action.objects.all().values('id', 'text', 'description')
-
-        # another approach was to use serialize as outside response
-        # 'to_do': serializers.ActionSerializer(models.Action.objects.all(), many=True).data
+        'to_do': serializers.ActionSerializer(models.Action.objects.all(), many=True).data
         # https://www.django-rest-framework.org/api-guide/serializers/
-        # this approach is much better
-    }
-    return Response(context) # status by default is 200
+        }
+        return Response(context)
 
-@api_view()
-def api_view(request, pk):
-    obj = get_object_or_404(models.Action, pk=pk)
-    # NOTE IN THIS FUNCTION WERE ONLY FOCUSING ON TRANSFERRING BACK A SERIALIZED OBJECT
-    # AND NOT WORRYING ABOUT TEMPLATES OR REDIRECTS
-    return Response({'data':serializers.ActionSerializer(obj).data})
+
+class APIGetView(APIView):
+    def get(self, request, pk):
+        obj = get_object_or_404(models.Action, pk=pk)
+        return Response({'data':serializers.ActionSerializer(obj).data})
 
 
 # FOR SOME REASON api_view decorator is bugging therefore use this
-from rest_framework.views import APIView
 
 class APIDeleteView(APIView):
     def delete(self, request, pk):
