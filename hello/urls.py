d3defc1661e30a67cc6b4f50b5c6800a318e775f
Kiel_Hyre <vinceadisip@gmail.com>
2021-12-03 11:33:21 +0800

    comment

--- hello/urls.py
+++ hello/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('index/', views.hello, name='hello-index'),
-]
+# urlpatterns = [
+#     path('index/', views.hello, name='hello-index'),
+# ]
