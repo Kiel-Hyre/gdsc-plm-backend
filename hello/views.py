12d1a53398e8c6cbda93f9409708c8367520cbe3
Kiel_Hyre <vinceadisip@gmail.com>
2021-12-03 12:10:08 +0800

    templates done

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# v1
+def hello(request):
+    return HttpResponse("Hello World")
+
+# v2
+# https://docs.djangoproject.com/en/3.2/topics/http/shortcuts/#django.shortcuts.render
 # def hello(request):
-#     return HttpResponse("Hello World")
+    # return render(request, 'hello.html')
+
+    # v3
+    # return render(request, 'hello.html', {'name': 'THE AUTHOR OF THIS APP'})
+
+    # v4
+    # https://docs.djangoproject.com/en/3.2/ref/templates/builtins/
+    # context = {
+    #     'name': 'Miguel', # change the name
+    #     'to_do': [
+    #         'homework',
+    #         'cleaning mugs',
+    #         'study',
+    #         'gdsc study jam',
+    #     ]
+    # }
+    # return render(request, 'hello.html', context)
